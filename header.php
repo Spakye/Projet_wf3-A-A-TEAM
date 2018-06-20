eccbae031612bf4422836bc34524767666cdc6e3
MGuelai <40197529+MGuelai@users.noreply.github.com>
2018-06-20 16:56:07 +0200

    header.php
    Intégration d'une barre de recherche dynamique qui renvoie vers la page mdp.php

--- header.php
+++ header.php
@@ -41,9 +41,11 @@ session_start();
 					</ul>				
 				</nav>
 				<div class="col-md-2 navbar navbar-expand-lg">
+				<form method="GET" action="mdr.php?produits=<?php $_GET['title']?>" >
 					<ul>
-					<input type="text" name="search"><i style="color:black;" class="fas fa-search"></i>
+					<input type="text" name="title"><i style="color:black;" class="fas fa-search"></i>
 					</ul>
+					</form>
 				</div>
 			</div>
 		</div>
