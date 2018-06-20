ed6531a30dde7e7d4eca8b8628a9b6a89784edd8
Aoced <aoced33@gmail.com>
2018-06-20 11:42:54 +0200

    new modif header

--- header.php
+++ header.php
@@ -18,8 +18,8 @@ session_start();
 				<div class="col-md-2">
 					<img src="https://placehold.it/150x50" alt="logo" class="logo">
 				</div>
-				<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<div class="col-md-8" id="navbarNav">
+				<nav class="col-md-8 navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-around">
+				<div id="navbarNav">
 					<ul class="navbar-nav">
 						<li class="nav-item active">
 							<a class="nav-link" href="#">CHAUSSURE</a>
