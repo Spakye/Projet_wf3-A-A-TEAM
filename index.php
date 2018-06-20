a58245925fdbd4baf7ae36e73735e3f6c219a0f1
RomainStut <39411840+RomainStut@users.noreply.github.com>
2018-06-20 10:33:27 +0200

    formulaire de contact

---
+++ index.php
@@ -0,0 +1,29 @@
+
+
+<body>
+    <h1 class="titre">Formulaire de contact</h1>
+    <form class="form">
+        <div class="form-group">
+          <label for="exampleInputEmail1" class="">Nom</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrez votre nom">
+          <small id="emailHelp" class="form-text text-muted"></small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Prénom</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Entrez votre prénom">
+        </div>        
+        <div class="form-group">
+            <label for="exampleInputEmail1">Adresse Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrez votre email">
+            <small id="emailHelp" class="form-text text-muted"></small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Mot de passe</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+          </div> 
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Votre message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+          </div>        
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+      </form>
