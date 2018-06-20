2f574acdc4c72e4e7997a9aed79d2968ec02b7f2
Spakye <leomagnac@gmail.com>
2018-06-20 11:05:51 +0200

    connection modif

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
-<?php include('header.php') ?>
+<?php include('header.php'); ?>
 
 <form method="POST" action="connection.php">
 	<input type="text" name="username" placeholder="Username or Email">
@@ -45,8 +45,7 @@ if (!empty($_POST)) {
 
 	
 }
-	
-?>
 
-</body>
-</html>
+
+include('footer.php');	
+?>
