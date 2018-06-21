b6cba480e4fa721d39fe22659952150cdc619e66
Spakye <leomagnac@gmail.com>
2018-06-21 09:55:24 +0200

    back

--- headerBack.php
+++ headerBack.php
@@ -10,9 +10,18 @@ session_start();
 	<title>BackOffice</title>	
 	<link href="https://fonts.googleapis.com/css?family=Brawler" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/backstyle.css">
 </head>
-<header>
-	<h1>Bienvenue <?= $_SESSION['username'] ?></h1>
-	yolo!
+<header class="container-fluid">
+	<div class="row">
+		<div class="col-md-6">
+			<p>Connecté en tant que : <?= $_SESSION['username'] ?></p>
+		</div>
+		<div class="col-md-6 text-right">
+			<a href="index.php">retour à la boutique</a>
+			<a href="gestion_articles.php">Gestion des articles</a>
+			<a href="gestion_utilisateurs.php">Gestion des utilisateurs</a>
+			<a href="gestion_index.php">Gestion de l'index</a>
+		</div>
+	</div>
 </header>
